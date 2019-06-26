302bc04b811a90853afe1c29d996130405a200f8
s.dmitriev <s.dmitriev@e-tender.biz>
2019-06-26 15:58:17 +0300

    fixes for cd procedures st1

--- etender_service.py
+++ etender_service.py
@@ -174,6 +174,7 @@ def get_helper_dictionary():
         #tender statuses
         u'період уточнень': u'active.enquiries',
         u'очікування пропозицій': u'active.tendering',
+        u'перший етап завершено': u'active.tendering',
         u'прекваліфікація': u'active.pre-qualification',
         u'блокування перед аукціоном': u'active.pre-qualification.stand-still',
         u'період аукціону': u'active.auction',
